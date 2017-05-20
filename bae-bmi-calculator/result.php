b5893b0bb7895334a4cc56fb22c402fe4baa2cbd
Koen <koensparreboom@gmail.com>
2017-05-20 12:36:28 +0200

    BMI calculator now uses a switch statement

--- bae-bmi-calculator/result.php
+++ bae-bmi-calculator/result.php
@@ -17,10 +17,6 @@
                 $length = $_POST["length"];
                 $weight = $_POST["weight"];
                 
-                if (is_int($length))
-                {
-                    echo "nice";
-                }
                 if (isset($_POST["name"]))
                 {
                     $name = $_POST["name"];
@@ -50,40 +46,34 @@
                 $bmi = round(($weight / ($length * $length))*10000, 1);
 
                 echo "Dat betekent dat uw BMI <u><b>$bmi</b></u> is.";
-                    
-                //$result;
-                //$color;
-           
             
-                if ($bmi < 18.5)
+                switch ($bmi)
                 {
-                    $result = "Helaas betekent dit dat u ondergewicht bent.";
-                    $color = "#00FFFF";
-                }
-                else if ($bmi >= 18.5 && $bmi < 25)
-                {
-                    $result = "Gefeliciteerd, dit betekent dat u een goed gewicht heeft!";
-                    $color = "#00FF00";
-                }
-                else if ($bmi >= 25 && $bmi < 30)
-                {
-                    $result = "Dit betekent dat u een klein beetje overgewicht bent.";
-                    $color = "#ffff00";
-                }
-                else if ($bmi >= 30 && $bmi < 35)
-                {
-                    $result = "U heeft klasse een obesitas.";
-                    $color = "#ffba1c";
-                }
-                else if ($bmi >= 35 && $bmi < 40)
-                {
-                    $result = "U heeft klasse twee obesitas.";
-                    $color = "#ff8d1c";
-                }
-                else if ($bmi >= 40)
-                {
-                    $result = "U heeft morbide obesitas.";
-                    $color = "#ff1c1c";
+                    case ($bmi < 18.5):
+                        $result = "Helaas betekent dit dat u ondergewicht bent.";
+                        $color = "#00FFFF";
+                        break;
+                        
+                    case ($bmi >= 18.5 && $bmi <25):
+                        $result = "Gefeliciteerd, dit betekent dat u een goed gewicht heeft!";
+                        $color = "#00FF00";
+                        break;
+                    case ($bmi >= 25 && $bmi < 30):
+                        $result = "Dit betekent dat u een klein beetje overgewicht bent.";
+                        $color = "#ffff00";
+                        break;
+                    case ($bmi >= 30 && $bmi < 35):
+                        $result = "U heeft klasse een obesitas.";
+                        $color = "#ffba1c";
+                        break;
+                    case ($bmi >= 35 && $bmi < 40):
+                        $result = "U heeft klasse twee obesitas.";
+                        $color = "#ff8d1c";
+                        break;
+                    case ($bmi >= 40);
+                        $result = "U heeft morbide obesitas.";
+                        $color = "#ff1c1c";
+                        break;
                 }
             
             
